e97bdef909f6d06e4954a2ef4a8e1a072b3f7bbb
Bryan <bryanortiz0297@gmail.com>
2021-10-05 13:04:53 -0600

    Local Links added UTM links paramether and fix date issue

--- src/app/main/dialogs/ActivateTripDialog.js
+++ src/app/main/dialogs/ActivateTripDialog.js
@@ -10,26 +10,26 @@ import Typography from '@material-ui/core/Typography';
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import * as Actions from 'app/auth/store/actions';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 // import { KeyboardDatePicker } from '@material-ui/pickers';
 import moment from 'moment';
 
-const tripStartDate = moment()
-const tripEndDate = moment().add(2000, 'days')
-// const tripStartDate2 = moment().format()
+const tripStartDate = moment();
+const tripEndDate = moment().add(2000, 'days');
+const queryParams = new URLSearchParams(window.location.search); // this is for the utm_id we can get all the utm paramethers
 
 const defaultFormState = {
-	tid: '',
+	tid: queryParams.get('utm_id') ? queryParams.get('utm_id') : '',
 	tripStartDate: tripStartDate,
-	tripEndDate: tripEndDate,
+	tripEndDate: tripEndDate
 };
 
 function ActivateTripDialog(props) {
 	const dispatch = useDispatch();
 	const user = useSelector(({ auth }) => auth.user);
-	const tripDialog = user.trip
+	const tripDialog = user.trip;
 	// console.log('==-=>',tripDialog);
-	
+
 	const { form, handleChange } = useForm(defaultFormState);
 
 	function canBeSubmitted() {
@@ -38,51 +38,51 @@ function ActivateTripDialog(props) {
 
 	function closeComposeDialog() {
 		console.log('in closeComposeDialog');
-		dispatch(Actions.closeTripDialog())
+		dispatch(Actions.closeTripDialog());
 	}
 
 	function handleSubmit(event) {
 		console.log('in handle submit1', form);
-		form['tripStartDate'] = tripStartDate
-		form['tripEndDate'] = tripEndDate
+		form['tripStartDate'] = tripStartDate;
+		form['tripEndDate'] = tripEndDate;
 		console.log('in handle submit2', form);
 		event.preventDefault();
 		dispatch(Actions.addTrip(form));
 		closeComposeDialog();
 	}
 
-		return (
-			<Dialog
-				classes={{
-					paper: 'm-24'
-				}}
-				open={tripDialog.openDialog}
-				onClose={closeComposeDialog}
-				fullWidth
-				maxWidth="xs"
-			>
-				<AppBar position="static" elevation={1}>
-					<Toolbar className="flex w-full">
-						<Typography variant="subtitle1" color="inherit">
-							Enter your Market ID continue
-						</Typography>
-					</Toolbar>
-				</AppBar>
-				<form noValidate onSubmit={handleSubmit} className="flex flex-col md:overflow-hidden">
-					<DialogContent classes={{ root: 'p-24' }}>
-						<TextField
-							fullWidth
-							id="tid"
-							name="tid"
-							label="Market ID"
-							variant="outlined"
-							value={form.tid}
-							onChange={handleChange}
-							required
-							autoFocus
-						/>
+	return (
+		<Dialog
+			classes={{
+				paper: 'm-24'
+			}}
+			open={tripDialog.openDialog}
+			onClose={closeComposeDialog}
+			fullWidth
+			maxWidth="xs"
+		>
+			<AppBar position="static" elevation={1}>
+				<Toolbar className="flex w-full">
+					<Typography variant="subtitle1" color="inherit">
+						Enter your Market ID continue
+					</Typography>
+				</Toolbar>
+			</AppBar>
+			<form noValidate onSubmit={handleSubmit} className="flex flex-col md:overflow-hidden">
+				<DialogContent classes={{ root: 'p-24' }}>
+					<TextField
+						fullWidth
+						id="tid"
+						name="tid"
+						label="Market ID"
+						variant="outlined"
+						value={form.tid}
+						onChange={handleChange}
+						required
+						autoFocus
+					/>
 
-						{/* <KeyboardDatePicker
+					{/* <KeyboardDatePicker
 							fullWidth
 							margin="normal"
 							id="tripStartDate"
@@ -107,36 +107,36 @@ function ActivateTripDialog(props) {
 							format="MM/DD/YYYY"
 							required
 						/> */}
-					</DialogContent>
-	
-					<DialogActions className="justify-between p-8">
-							<div className="px-16">
-								<Button
-									component={Link}
-									to={'/pages/faq'}
-									variant="contained"
-									color="primary"
-									onClick={()=>console.log('in cancel button')}
-									type="submit"
-								>
-									Cancel
-								</Button>
-							</div>
-							<div className="px-16">
-								<Button
-									variant="contained"
-									color="primary"
-									type="submit"
-									onClick={handleSubmit}
-									disabled={!canBeSubmitted()}
-								>
-									Next
-								</Button>
-							</div>
-						</DialogActions>
-				</form>
-			</Dialog>
-		);
+				</DialogContent>
+
+				<DialogActions className="justify-between p-8">
+					<div className="px-16">
+						<Button
+							component={Link}
+							to={'/pages/faq'}
+							variant="contained"
+							color="primary"
+							onClick={() => console.log('in cancel button')}
+							type="submit"
+						>
+							Cancel
+						</Button>
+					</div>
+					<div className="px-16">
+						<Button
+							variant="contained"
+							color="primary"
+							type="submit"
+							onClick={handleSubmit}
+							disabled={!canBeSubmitted()}
+						>
+							Next
+						</Button>
+					</div>
+				</DialogActions>
+			</form>
+		</Dialog>
+	);
 }
 
 export default ActivateTripDialog;
